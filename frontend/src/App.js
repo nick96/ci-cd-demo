7d1659935f1b9d60dd8a493472ab86a7bd6308e9
Nick Spain <nicholas.spain96@gmail.com>
2019-07-26 23:18:53 +1000

    Implement functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { conversion: "toKg", value: 0.0, converted: undefined };
+  }
+
+  handleInputChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleChoiceChange = event => {
+    this.setState({ conversion: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    let url = "http://localhost:8000/conversion?";
+    switch (this.state.conversion) {
+      case "toKg": {
+        url += `from=lb&to=kg&value=${this.state.value}`;
+        break;
+      }
+      case "toLb": {
+        url += `from=kg&to=lb&value=${this.state.value}`;
+        break;
+      }
+      default:
+        console.error(`Unhandled conversion ${this.state.conversion}`);
+    }
+    fetch(url)
+      .then(resp => {
+        resp.json().then(json => this.setState({ converted: json.value }));
+      })
+      .catch(err => console.error(`Error: ${err}`));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmit}>
+          <div className="value-input">
+            <label>
+              Value:
+              <input
+                type="text"
+                name="value"
+                value={this.state.value}
+                onChange={this.handleInputChange}
+              />
+            </label>
+          </div>
+          <div className="options">
+            <input
+              type="radio"
+              name="conversion"
+              value="toKg"
+              onChange={this.handleChoiceChange}
+              checked={this.state.conversion === "toKg"}
+            />
+            To Kg
+            <input
+              type="radio"
+              name="conversion"
+              value="toLb"
+              onChange={this.handleChoiceChange}
+              checked={this.state.conversion === "toLb"}
+            />
+            To Lb
+          </div>
+
+          <input type="submit" value="submit" />
+        </form>
+
+        <div>
+          {this.state.converted}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
