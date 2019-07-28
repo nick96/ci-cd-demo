03ff5159b26c1ff54c1f83b335cd37469db26739
Nick Spain <nicholas.spain96@gmail.com>
2019-07-28 22:17:45 +1000

    Format tf file

--- api/terraform/main.tf
+++ api/terraform/main.tf
@@ -4,28 +4,25 @@ variable "PIPELINE_SSHKEY" {}
 
 variable "PERSONAL_SSHKEY" {}
 
-
 provider "digitalocean" {
   token = "${var.DO_TOKEN}"
 }
 
 resource "digitalocean_ssh_key" "pipeline" {
-  name = "CircleCI pipeline"
+  name       = "CircleCI pipeline"
   public_key = "${file("${var.PIPELINE_SSHKEY}")}"
 }
 
 resource "digitalocean_ssh_key" "personal" {
-    name = "Nick - Personal SSH Key"
-    public_key = "${file("${var.PERSONAL_SSHKEY}")}"
+  name       = "Nick - Personal SSH Key"
+  public_key = "${file("${var.PERSONAL_SSHKEY}")}"
 }
 
-
-
 resource "digitalocean_droplet" "api" {
-  image  = "ubuntu-18-04-x64"
-  name   = "api-1"
-  region = "sgp1"
-  size   = "s-1vcpu-1gb"
+  image    = "ubuntu-18-04-x64"
+  name     = "api-1"
+  region   = "sgp1"
+  size     = "s-1vcpu-1gb"
   ssh_keys = ["${digitalocean_ssh_key.pipeline.fingerprint}", "${digitalocean_ssh_key.personal.fingerprint}"]
 }
 
