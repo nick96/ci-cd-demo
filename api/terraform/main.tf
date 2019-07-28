df861e8db5e18a896f55e506b1470a85f710ff10
Nick Spain <nicholas.spain96@gmail.com>
2019-07-28 22:32:25 +1000

    Remove firewall

--- api/terraform/main.tf
+++ api/terraform/main.tf
@@ -37,53 +37,6 @@ resource "digitalocean_record" "www_api" {
   value  = "${digitalocean_droplet.api.ipv4_address }"
 }
 
-resource "digitalocean_firewall" "www_api" {
-  name = "only-22-80-and-443"
-
-  droplet_ids = ["${digitalocean_droplet.api.id}"]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "icmp"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol   = "tcp"
-    port_range = "1-65535"
-    destination_addresses   = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol   = "udp"
-    port_range = "1-65535"
-    destination_addresses   = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol   = "icmp"
-    port_range = "1-65535"
-    destination_addresses   = ["0.0.0.0/0", "::/0"]
-  }
-}
-
 output "fqdn" {
   value = "${digitalocean_record.www_api.fqdn}"
 }
