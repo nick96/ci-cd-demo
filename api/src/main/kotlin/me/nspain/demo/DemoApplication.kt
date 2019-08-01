3ddd6af567c2f871e89b371c55a5f8a502c7b1b8
Nick Spain <nicholas.spain96@gmail.com>
2019-08-01 15:24:21 +1000

    Fix linting issues in kotlin files

--- api/src/main/kotlin/me/nspain/demo/DemoApplication.kt
+++ api/src/main/kotlin/me/nspain/demo/DemoApplication.kt
@@ -3,72 +3,76 @@ package me.nspain.demo
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 open class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
 
 @Configuration
 @EnableWebMvc
-open class WebConfig: WebMvcConfigurer {
-	override fun addCorsMappings(registry: CorsRegistry) {
-		registry.addMapping("/**");
-	}
+open class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
 }
 
 @RestController
 @RequestMapping("/conversion")
 class ConversionController {
 
-	val conversions = listOf("lb", "kg")
+    val conversions = listOf("lb", "kg")
 
-	val TO_KG = 0.45359237
-	val TO_LB = 2.20462262185
+    val TO_KG = 0.45359237
+    val TO_LB = 2.20462262185
 
-	val logger = LoggerFactory.getLogger(this::class.java)
+    val logger = LoggerFactory.getLogger(this::class.java)
 
+    @GetMapping
+    fun getConversion(@RequestParam from: String, @RequestParam to: String, @RequestParam value: Double): ResponseEntity<Response> {
+        logger.info("Received request with from='$from', to='$to' and value='$value'")
+        if (!conversions.containsAll(listOf(from, to))) {
+            logger.info("Invalid value for from or to")
+            return ResponseEntity.badRequest().build()
+        }
 
-	@GetMapping
-	fun getConversion(@RequestParam from: String, @RequestParam to: String, @RequestParam value: Double): ResponseEntity<Response> {
-		logger.info("Received request with from='$from', to='$to' and value='$value'")
-		if (!conversions.containsAll(listOf(from, to))) {
-			logger.info("Invalid value for from or to")
-			return ResponseEntity.badRequest().build()
-		}
+        val converted = when (from) {
+            "kg" -> fromKg(to, value)
+            "lb" -> fromLb(to, value)
+            else -> {
+                return ResponseEntity.badRequest().build()
+            }
+        }!!
+        logger.info("Converted $value$from to $converted$to")
 
-		val converted = when (from) {
-			"kg" -> fromKg(to, value)
-			"lb" -> fromLb(to, value)
-			else -> { return ResponseEntity.badRequest().build() }
-		}!!
-		logger.info("Converted $value$from to $converted$to")
+        val resp = Response("$from to $to", converted)
+        return ResponseEntity.ok(resp)
+    }
 
-		val resp = Response("$from to $to", converted)
-		return ResponseEntity.ok(resp)
-	}
+    private fun fromLb(to: String, value: Double): Double? {
+        return when (to) {
+            "kg" -> value * TO_KG
+            else -> null
+        }
+    }
 
-	private fun fromLb(to: String, value: Double): Double? {
-		return when (to) {
-			"kg" -> value * TO_KG
-			else -> null
-		}
-	}
-
-	private fun fromKg(to: String, value: Double): Double? {
-		return when (to) {
-			"lb" -> value * TO_LB
-			else -> null
-		}	}
+    private fun fromKg(to: String, value: Double): Double? {
+        return when (to) {
+            "lb" -> value * TO_LB
+            else -> null
+        }
+    }
 }
 
 data class Response(val conversion: String, val value: Double)
-
